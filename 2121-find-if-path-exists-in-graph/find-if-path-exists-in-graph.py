6dd342ce530a9913ab52b3cf8752be937e95693a
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-02-27 00:00:01 +0530

    Time: 330 ms (81.18%) | Memory: 96.7 MB (67.78%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -9,15 +9,16 @@ class Solution:
             graph[v].append(u)
         visited=set()
         visited.add(source)
-        stack=[source]
-        while stack:
-            node=stack.pop()
+        q=deque()
+        q.append(source)
+        while q:
+            node=q.popleft()
             if node==destination:
                 return True
             for nei_node in graph[node]:
                 if nei_node not in visited:
                     visited.add(nei_node)
-                    stack.append(nei_node)
+                    q.append(nei_node)
                     if nei_node==destination:
                         return True
         return False
