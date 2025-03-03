0264f2f680bc301300cff2822be2673092823e9b
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-03-04 02:56:35 +0530

    Time: 46 ms (37.88%) | Memory: 17.7 MB (64.13%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def __init__(self):
+        self.dp={}
+    def fib(self, n: int) -> int:
+        if n<=1:
+            return n
+        if n in self.dp:
+            return self.dp[n]
+        self.dp[n] = self.fib(n - 1) + self.fib(n - 2)
+        return self.dp[n]
