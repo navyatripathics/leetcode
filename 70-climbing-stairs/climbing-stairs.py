1fe505fa001cd8e8a6ac779dd435879083828cf7
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-03-05 10:22:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (78.21%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -4,9 +4,9 @@ class Solution:
             return 0
         if n==1:
             return 1
-        dp=[-1]*(n+1)
-        dp[0]=1
-        dp[1]=1
+        dp=[-1]*(n+1) 
+        dp[0]=1 #one way to stay at the ground
+        dp[1]=1 #one step to go from 0 to 1
         for i in range(2,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
+            dp[i]=dp[i-1]+dp[i-2] 
         return dp[n]
