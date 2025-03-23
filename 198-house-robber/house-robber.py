cf1a37a3a3372b0ff0b7d41e251035eb06004804
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-03-23 21:20:41 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.49%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -7,12 +7,14 @@ class Solution:
             return 0
         if n == 1:
             return nums[0]
-        
-        dp = [-1] * n
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        
-        for i in range(2, n):
-            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
-        
-        return dp[-1]
+
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = nums[0]
+
+        for i in range(2, n + 1):
+            rob = nums[i - 1] + (dp[i - 2] if i - 2 >= 0 else 0)
+            not_rob = dp[i - 1]
+            dp[i] = max(rob, not_rob)
+
+        return dp[n]
