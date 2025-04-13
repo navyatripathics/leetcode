81912ef48a641b2e9916c5d98b439cdbebb27ab7
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-04-13 21:08:35 +0530

    Time: 1337 ms (9.52%) | Memory: 21.6 MB (22.93%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,4 +1,4 @@
-class Solution:
+'''class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n=len(coins)
         dp=[[-1 for _ in range(amount+1)]for _ in range(n)]
@@ -16,4 +16,25 @@ class Solution:
             dp[ind][target]=min(take,not_take)
             return dp[ind][target]
         ans = f(n - 1, amount)
-        return -1 if ans == float('inf') else ans
+        return -1 if ans == float('inf') else ans'''
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        n=len(coins)
+        dp=[[0 for _ in range(amount+1)]for _ in range(n)]
+        for i in range(amount+1):
+            if i%coins[0]==0:
+                dp[0][i]=i//coins[0]
+            else:
+                dp[0][i]=float('inf')
+        for ind in range(1,n):
+            for target in range(amount+1):
+                not_take=dp[ind-1][target]
+                take=float('inf')
+                if coins[ind]<=target:
+                    take=1+dp[ind][target-coins[ind]]
+                dp[ind][target]=min(take,not_take)
+        ans=dp[n-1][amount]
+        if ans==float('inf'):
+            return -1
+        else:
+            return ans
