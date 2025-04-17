23a4773c86d610ad7aa847116b34f71c256cef85
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-04-17 06:57:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (15.10%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,19 +1,13 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         n=len(nums)
-        map=[False]*n
-        ds=[]
         res=[]
-        def f(ds,map):
-            
-            if len(ds)==n:
-                res.append(ds[:])
-            for i in range(n):
-                if nums[i] not in ds:
-                    ds.append(nums[i])
-                    map[i]=True
-                    f(ds,map)
-                    ds.pop()
-                    map[i]=False
-        f(ds,map)
+        def f(ind,nums):
+            if ind==n:
+                res.append(nums[:])
+            for i in range(ind,n):
+                nums[ind],nums[i]=nums[i],nums[ind] #swap
+                f(ind+1,nums)
+                nums[ind],nums[i]=nums[i],nums[ind] #undo the swap
+        f(0,nums)
         return res
