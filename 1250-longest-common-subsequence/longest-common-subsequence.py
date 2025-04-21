e29eea2f4eeac49fef41cffab5369cfce2690315
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-04-21 11:10:51 +0530

    Time: 513 ms (43.19%) | Memory: 43.7 MB (26.11%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -19,10 +19,6 @@ class Solution:
         n=len(text1)
         m=len(text2)
         dp=[[0 for _ in range(m+1)]for _ in range(n+1)]
-        for i in range(n):
-            dp[i][0]=0
-        for j in range(m):
-            dp[0][j]=0
         for i in range(1,n+1):
             for j in range(1,m+1):
                 if text1[i-1]==text2[j-1]:
