be46c5901c8183bf14e51e82a12d45a5ff0aa64e
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-05-27 14:46:32 +0530

    Time: 106 ms (90.15%) | Memory: 18 MB (49.25%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,19 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        n=len(s)
+        left=0
+        hash={}
+        maxf=0
+        maxlen=0
+        for right in range(left,n):
+            if s[right] in hash:
+                hash[s[right]]+=1
+            else:
+                hash[s[right]]=1
+            maxf=max(maxf,hash[s[right]])
+            if (right-left+1-maxf)<=k:
+                maxlen=max(maxlen,right-left+1)
+            else:
+                hash[s[left]]-=1
+                left+=1
+        return maxlen
