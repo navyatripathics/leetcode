4afe576937ac2be7f18dae7c62cc0b2ec729415d
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-07-07 22:27:46 +0530

    Time: 2 ms (86.64%) | Memory: 18 MB (5.73%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,22 +1,26 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        minute=0
+        row=len(grid)
+        col=len(grid[0])
         q=deque()
-        visited=set()
-        n,m=len(grid),len(grid[0])
-        for i in range(n):
-            for j in range(m):
+        fresh=0
+        for i in range(row):
+            for j in range(col):
                 if grid[i][j]==2:
-                    q.append((i,j,0))
-                    visited.add((i,j))
-        direction=[(0,1),(0,-1),(1,0),(-1,0)]
-        while q:
-            row,col,minute=q.popleft()
-            for dr,dc in direction:
-                new_row,new_col=dr+row,dc+col
-                if 0<=new_row<n and 0<=new_col<m and grid[new_row][new_col]==1 and (new_row,new_col) not in visited:
-                    grid[new_row][new_col]=2
-                    q.append((new_row,new_col,minute+1))
-                    visited.add((new_row,new_col))
-        return -1 if any(1 in row for row in grid) else minute
+                    q.append((i,j))
+                if grid[i][j]==1:
+                    fresh+=1
+        directions={(-1,0),(1,0),(0,-1),(0,1)}
+        minute=0
+        while q and fresh>0:
+            for _ in range(len(q)):
+                r,c=q.popleft()
+                for dr,dc in directions:
+                    newr,newc=r+dr,c+dc
+                    if 0<=newr<row and 0<=newc<col and grid[newr][newc]==1:
+                        grid[newr][newc]=2
+                        q.append((newr,newc))
+                        fresh-=1
+            minute+=1
+        return minute if fresh==0 else -1
