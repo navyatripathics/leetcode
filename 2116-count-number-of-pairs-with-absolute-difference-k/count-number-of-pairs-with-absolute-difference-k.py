0e418afc3491c7acf14112e74b7e3cae1e6a9849
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-08-04 00:02:31 +0530

    Time: 2 ms (91.47%) | Memory: 17.8 MB (38.70%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.py
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.py
@@ -1,8 +1,14 @@
 class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
+        hashmap={}
         count=0
-        for i in range(len(nums)):
-            for j in range(i,len(nums)):
-                if abs(nums[i]-nums[j])==k:
-                    count+=1
+        for i in nums:
+            if i not in hashmap:
+                hashmap[i]=1
+            else:
+                hashmap[i]+=1
+            if i - k in hashmap:
+                count += hashmap[i - k]
+            if i + k in hashmap:
+                count += hashmap[i + k]
         return count
