e75fa3f6071725f689141410a0774dacda68137e
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-08-06 11:28:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (12.02%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        result=[]
+        def traverse(node):
+            if node:
+                traverse(node.left)
+                result.append(node.val)
+                traverse(node.right)
+            return result
+        traverse(root)
+            
+        return result
