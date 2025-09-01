b4876678df872a33feb15cd95708cbbec27cbfe1
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-09-01 21:41:16 +0530

    Time: 300 ms (47.15%) | Memory: 18.8 MB (83.37%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,20 +1,19 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        wordSet=set(wordList)
-        if endWord not in wordSet:
+        if endWord not in wordList:
             return 0
+        w=set(wordList)
         q=deque([(beginWord,1)])
         visited=set()
         visited.add(beginWord)
         while q:
-            word,step=q.popleft()
-            for i in range(len(word)):
-                for j in string.ascii_lowercase:
-                    newword=word[:i]+j+word[i+1:]
-                    
-                    if newword==endWord:
-                            return step+1
-                    if newword in wordSet and newword not in visited:
-                        q.append((newword,step+1))
+            node,level=q.popleft()
+            if node==endWord:
+                return level
+            for i in range(len(node)):
+                for c in "abcdefghijklmnopqrstuvwxyz":
+                    newword=node[:i]+c+node[i+1:]
+                    if newword in w and newword not in visited:
                         visited.add(newword)
+                        q.append((newword,level+1))
         return 0
