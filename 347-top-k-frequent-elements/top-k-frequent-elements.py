890e5a8a10ae355f82e50a60903a6cce05f90fce
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-09-08 14:18:12 +0530

    Time: 3 ms (90.56%) | Memory: 21.2 MB (62.22%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,13 @@
-import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         d={}
-        a=[]
-        heap=[]
-        for i in nums:
-            if i not in d:
-                d[i]=1
+        for num in nums:
+            if num not in d:
+                d[num]=1
             else:
-                d[i]+=1
-        for key,values in d.items():
-            heapq.heappush(heap,(-values,key))
+                d[num]+=1
+        sorted_d=sorted(d.items(),key=lambda item:item[1],reverse=True)
+        l=[]
         for i in range(k):
-            a.append(heapq.heappop(heap)[1])
-        return a
-        
+            l.append(sorted_d[i][0])
+        return l
