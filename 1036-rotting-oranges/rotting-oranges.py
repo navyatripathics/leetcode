b322101b99fbee9dc05d2e9deec2c0133742e2a2
Navya Tripathi <126502481+navyatripathics@users.noreply.github.com>
2025-10-30 21:03:21 +0530

    Time: 4 ms (47.04%) | Memory: 18 MB (25.99%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,26 +1,26 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        row=len(grid)
+        rows=len(grid)
         col=len(grid[0])
-        q=deque()
+        queue=deque()
         fresh=0
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j]==2:
-                    q.append((i,j))
-                if grid[i][j]==1:
+        for r in range(rows):
+            for c in range(col):
+                if grid[r][c]==2:
+                    queue.append((r,c))
+                elif grid[r][c]==1:
                     fresh+=1
-        directions={(-1,0),(1,0),(0,-1),(0,1)}
-        minute=0
-        while q and fresh>0:
-            for _ in range(len(q)):
-                r,c=q.popleft()
-                for dr,dc in directions:
-                    newr,newc=r+dr,c+dc
-                    if 0<=newr<row and 0<=newc<col and grid[newr][newc]==1:
-                        grid[newr][newc]=2
-                        q.append((newr,newc))
+        minutes=0
+        directions=[(1,0),(-1,0),(0,1),(0,-1)]
+        while queue and fresh>0:
+            for _ in range(len(queue)):
+                r,c=queue.popleft()
+                for dr, dc in directions:
+                    nr,nc=r+dr,c+dc
+                    if 0<=nr<rows and 0<=nc<col and grid[nr][nc]==1:
+                        grid[nr][nc]=2
                         fresh-=1
-            minute+=1
-        return minute if fresh==0 else -1
+                        queue.append((nr,nc))
+            minutes+=1
+        return minutes if fresh==0 else -1
